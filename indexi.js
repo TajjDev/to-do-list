aeb925a3045897c8e566da2d886d8bccaa26f655
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2024-03-01 15:38:26 +0100

    Add files via upload

---
+++ indexi.js
@@ -0,0 +1,40 @@
+// let task = document.getElementById("task") as HTMLInputElement;
+// let btnsub = document.getElementById("btnsub") as  HTMLInputElement;
+// let theTaskInput = document.querySelector("#theTaskInput") as HTMLLIElement
+// let donne = document.getElementById('donne') as HTMLElement
+// // let btn = document.getElementById("btn") as HTMLButtonElement
+// // let btn2 = document.getElementById("btn2") as HTMLButtonElement
+// let arr:string[] = []
+// function submit() {
+//    arr.push(task.value.toLocaleUpperCase())
+//    console.log(arr);
+//    if (theTaskInput.childElementCount>0) {
+//     theTaskInput.replaceChildren();
+//    }
+//    for (let index = 0; index < arr.length; index++) {
+//       let li = document.createElement('li')
+//       li.innerText = task.value
+//     theTaskInput.appendChild(li)
+//    //  theTaskInput.innerHTML += `<li>${arr[index] }</li>
+//    // `
+//    let btn = document.createElement('button')
+//    let btn2 = document.createElement('input')
+//    btn.innerHTML = "Edit Task"
+//    console.log(btn);
+   
+//    btn2.setAttribute("type","checkbox")
+//    btn2.setAttribute("id","me")
+//    // btn2.setAttribute("onclick","men()")
+//    li.appendChild(btn)
+//    li.appendChild(btn2)
+//    // function men(){
+//    //    for (let index = 0; index < arr.length; index++) {
+//    //      btn.style.display = "none"
+//    // }
+//    // }
+//    // men()
+//    }
+   
+// }
+
+
---
+++ script.ts
@@ -0,0 +1,33 @@
+let taskinput = document.getElementById("task") as HTMLInputElement;
+let btnsub = document.getElementById("btnsub") as  HTMLInputElement;
+let theTaskInput = document.querySelector("#theTaskInput") as HTMLOListElement
+let donne = document.getElementById('donne') as HTMLElement
+let taskArray:string[] = []
+function submit() {
+    if (taskinput.value !== '') {
+        taskArray.push(taskinput.value)
+        taskinput.value = ''
+        displayTask()
+    }
+}
+const displayTask = ()=> {
+    let content:string = ""
+for (let index = 0; index < taskArray.length; index++) {
+    content += `<li>${taskArray[index]}<br><button onclick="editTask(${index})">Edit Task</button><button onclick=" deleteTask(${index})">Task Completed</button></l1>`
+}
+    theTaskInput.innerHTML = content 
+}
+
+const deleteTask = (index:number):void=>{
+    taskArray.splice(index,1)
+    displayTask()
+}
+
+const editTask = (index:number):void=>{
+    let a = prompt('ENTER A NEW NAME FOR YOUR TASK')
+    if (a !== '') {
+    taskArray.splice(index,1,`${a}`)
+    displayTask()
+    }
+    
+}
